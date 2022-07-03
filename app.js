1929969093e517699d90c9f6a45a601c1c876c05
Dane85 <wattsd1985@gmail.com>
2022-07-03 11:55:00 -0400

    updates

--- app.js
+++ app.js
@@ -21,9 +21,10 @@ app.get('/', function(req, res) {
   res.render('pages/index');
 });
 
-// about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
+
+//Capture All 404 errors
+app.use(function (req,res,next){
+  res.status(404).send('Unable to find the requested resource!');
 });
 
 //listening port
